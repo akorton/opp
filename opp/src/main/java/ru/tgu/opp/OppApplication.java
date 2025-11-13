c5f62e6c40e203f097983c46dfcc2049dd735df5
andy <akortysh@yandex.ru>
2025-11-13 19:29:36 +0700

    Init

---
+++ opp/src/main/java/ru/tgu/opp/OppApplication.java
@@ -0,0 +1,13 @@
+package ru.tgu.opp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OppApplication.class, args);
+	}
+
+}
