86ad2404efd35a8c220966859398542a86448cf6
andy <akortysh@yandex.ru>
2026-06-14 16:42:33 +0700

    Static analisys

---
+++ opp/src/main/java/ru/tgu/opp/vulnerabilities/SHA1Bean.java
@@ -0,0 +1,11 @@
+package ru.tgu.opp.vulnerabilities;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SHA1Bean {
+
+    public MessageDigest getSHA1() throws NoSuchAlgorithmException {
+        return MessageDigest.getInstance("SHA-1");
+    }
+}
